3c4df7d5486827aa649615ab0dea0aff2112f6e5
JoaoSurita <joaopedrosurita490@gmail.com>
2026-02-23 11:09:00 -0300

    Orientacao a Objeto

---
+++ TPII/Heranca/Program.cs
@@ -0,0 +1,49 @@
+﻿
+Aluno aluno1 = new Aluno("Paulo", "111.111.111-11", "0201392511020");
+
+Console.WriteLine($"Nome:{aluno1.Nome}\nCPF:{aluno1.Cpf}\nRA:{aluno1.Ra}");
+Console.WriteLine();
+
+Console.Write("Digite o nome do Professor: ");
+var nomeProf = Console.ReadLine();
+
+Console.Write("Digite o CPF do Professor: ");
+var cpfProf = Console.ReadLine();
+
+Console.Write("Digite a Titulação do Professor: ");
+var titulacaoProf = Console.ReadLine();
+
+Professor prof1 = new Professor(nomeProf, cpfProf, titulacaoProf);
+Professor.Exibir(prof1);
+
+
+public class Pessoa
+{
+    public Pessoa(string? nome, string? cpf)
+    {
+        Nome = nome;
+        Cpf = cpf;
+    }
+    public string? Nome { get; set; }
+    public string? Cpf { get; set; }
+}
+
+public class Aluno : Pessoa
+{
+    public Aluno(string? nome, string? cpf, string? ra) : base(nome, cpf)
+    {
+        Ra = ra;
+    }
+    public string? Ra { get; set; }
+}
+
+public class Professor : Pessoa
+{
+    public Professor(string? nome, string? cpf, string? titulacao) : base(nome, cpf)
+    {
+        Titulacao = titulacao;
+    }
+    public string? Titulacao { get; set; }
+
+    public static void Exibir(Professor prof) => Console.WriteLine($"Nome:{prof.Nome}\nCPF:{prof.Cpf}\nRA:{prof.Titulacao}");
+}
---
+++ TPII/OrientacaoObj/Program.cs
@@ -0,0 +1,36 @@
+﻿// Usando
+using OrientacaoObj;
+
+Produto prod1 = new Produto();
+prod1.Nome = "Caderno";
+prod1.Preco = 24.99;
+
+Produto prod2 = new Produto("Lápis", 1.50);
+
+// objeto prod1
+Console.WriteLine("Nome: " + prod1.Nome);
+Console.WriteLine($"Preço: {prod1.Preco}");
+
+// objeto prod2
+Console.WriteLine("Nome: " + prod2.Nome);
+Console.WriteLine($"Preço: {prod2.Preco}");
+
+// objeto Produto2
+Produto2 prod3 = new Produto2("Borracha", 4.50, 100);
+prod3.Exibir();
+
+// Declarando
+public class Produto
+{
+
+    public Produto() { }
+    // Método Constutor
+    public Produto(string nome, double Preco)
+    {
+        Nome = nome;
+        // Caso coloque o atributo igual o nome do parâmetro
+        this.Preco = Preco;
+    }
+    public string? Nome { get; set; }
+    public double Preco { get; set; }
+}
