71ea7ab32bbbb1d752c5fa62daf1220402d0eccc
JOAO PEDRO SURITA <0201392511020>
2026-03-30 11:03:43 -0300

    Aula sobre exceções

---
+++ TPII/Excecao/Excecao/Program.cs
@@ -0,0 +1,31 @@
+﻿try
+{
+    Console.WriteLine("Digite o Dividendo");
+    int dividendo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Digite o Divisor");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+
+    var resultado = dividendo / divisor;
+    Console.WriteLine($"O resultado de {dividendo}/{divisor} = {resultado}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Entre com um valor inteiro");
+}
+catch (Exception ex) when (ex.Message.Contains("format"))
+{
+    Console.WriteLine($"Entre com um valor inteiro - when:{ex.Message}");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Divisor não pode ser zero");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Problema na divisão: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Divisão concluída");
+}
